d9f624d6fb47f668fd158d537ced912409930f2a
AdnanCigtekin <cigtekinadnan@gmail.com>
2020-05-15 23:48:57 +0300

    Output is colorful version also

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends React.Component {
 
   constructor(props){
@@ -9,14 +10,78 @@ class App extends React.Component {
     
     this.state = {
       myInput: "",
-      myOutput: ""
+      myOutput: "",
+      outputElements: "",
+      finaloutput:""
     }
     this.prepareOutput = this.prepareOutput.bind(this);
     this.updateData= this.updateData.bind(this);
-
+    this.writeMyOutput= this.writeMyOutput.bind(this);
     //For changing the background of the page lol
     document.body.style = 'background: #282c34;';
   };
+
+  //TODO: Find a way to Write everything in one line as objects
+  //TODO: Changing "Aposthrope" class to a general purpose class might be a good idea.
+  writeMyOutput(){
+
+    let StringElements = "'\""
+    let TabOpeners = "{["
+    let TabClosers = "]}"
+
+    let tempElements = []
+    this.state.myOutput.split("\n").forEach(element => {
+
+        let inString = false;
+        let passedColon = false;
+        element.split("").forEach(index => {
+          if(index === "\t"){
+            tempElements.push(<span className="App-tab">_______</span>);
+          }
+          else if(index === ","){
+            tempElements.push(<span className="App-comma">,</span>);
+          }
+          else if(StringElements.includes(index)){
+            tempElements.push(<span className="App-string">{index}</span>);
+            inString = !inString;
+          }
+          else if(TabClosers.includes(index) || TabOpeners.includes(index)){
+            tempElements.push(<span className="App-paranth">{index}</span>);
+          }
+          else{
+            if(inString){
+              tempElements.push(<span className="App-string">{index}</span>);
+            }else{
+              if(!passedColon){
+                tempElements.push(<span className="App-normal-character">{index}</span>);
+                if(index === ":"){
+                  passedColon = true;
+                }
+              }else{
+                tempElements.push(<span className="App-var">{index}</span>);
+              }
+            }
+          }
+
+        });
+        // if(element[0] === "\t"){
+        //   tempElements.push(<span className="App-tab">___</span>);
+        //   tempElements.push(<span className="App-tab"></span>);
+        // }
+       // tempElements.push(<span className="App-normal-character">{element}</span>);
+        tempElements.push(<br></br>);
+      
+    });
+
+    //tempElements.push(<span className="App-aposth">fsafsafa</span>)
+    this.setState({outputElements: tempElements})
+
+
+
+  }
+
+
+
 
   prepareOutput(){
     let maxIndex = this.state.myInput.length;
@@ -79,7 +144,11 @@ class App extends React.Component {
 
       index++;
     }
-    this.setState({myOutput: newOutput})
+    this.setState({myOutput: newOutput},
+      () => {
+        // Generating output
+      this.writeMyOutput();
+      })
   }
 
   updateData(e){
@@ -108,7 +177,11 @@ render(){
 
         </table> 
         <button onClick={this.prepareOutput} className = "App-button">Beautify</button>
+
       </center>
+      <div className="App-text-container">
+          <span className="App-list">{this.state.outputElements}</span>
+      </div>
     </div>
   );
 }
